4378719bff5a026e46882be40ef6c03f01bcce6e
leosj29 <leosj29@gmail.com>
2022-08-31 16:06:00 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebPack = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const CopyWebPack = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  output: {
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          sources: false
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /styles.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /styles.css$/,
+        use: [MiniCssExtract.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  // optmization: {},
+  plugins: [
+    new HtmlWebPack({
+      title: 'Mi Webpacl App',
+      template: './src/index.html'
+    }),
+    new MiniCssExtract({
+      filename: '[name].css',
+      ignoreOrder: false
+    }),
+    new CopyWebPack({
+      patterns: [
+        { from: "src/assets/", to: "assets/" }
+      ]
+    })
+  ]
+}
